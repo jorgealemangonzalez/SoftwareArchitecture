8f3e625619daa07d3a9cf930c153893341e4044c
guinii <guinartarnau@gmail.com>
2016-01-20 13:19:58 +0100

    Paso 01 tutorial c++

---
+++ tutorial/exemple_polimorfisme.cxx
@@ -0,0 +1,11 @@
+/* 
+  - streams de la llibreria estàndard per escriure a consola
+  - namespace std::nom
+ */
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "Hola mon" << std::endl;
+	return 0;
+}
