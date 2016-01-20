f1dce1119bd265f39a88fd261592f9d8bbb0f31f
guinii <guinartarnau@gmail.com>
2016-01-20 14:34:20 +0100

    Paso 15 tutorial c++

--- tutorial/exemple_polimorfisme.cxx
+++ tutorial/exemple_polimorfisme.cxx
@@ -1,9 +1,21 @@
 /* 
-   - Estem fent el canvi de vector a llista :
-     Ara que iterem amb iteradors i tenim el typdef, fer el canvi només
-     implica: (a) canviar el typedef i (b) canviar l'include a <list>
-     (Aquesta és la grandesa dels iteradors: desacoblar el codi d'iteració 
-     de l'estructura de dades)
+   - usem els accessors d'escriptura i de lectura de l'atribut _nom
+
+   En resum hem après:
+   - com escriure per consola fent servir <iostream>
+   - simbols de la libreria localitzats al namespace std::
+   - les classes acaben en ;
+   - el mètodes i atributs per defecte són private
+   - com fer subclasses: "Sub : public Super"
+   - perquè serveixen els mètodes virtuals
+   - polimorfisme
+   - usar #ifndef als headers per evitar redefinicions
+   - pas de paràmetres per referència
+   - col.leccions estàndars vector i list
+   - typdefs
+   - iteradors
+   - std::string
+   - ús d'accessors
  */
 
 #include <list>
@@ -17,20 +29,28 @@ int main(void)
 
 	// introduïm un de cada tipus
 	Animal unAnimal;
+	unAnimal.nom("Pikatxu");
 	animals.push_back(&unAnimal);
 
 	Elefant unElefant;
+	unElefant.nom("Jumbo l'Orellut");
 	animals.push_back(&unElefant); 
 
 	Granota unaGranota;
+	unaGranota.nom("Gustavo el Reporter");
 	animals.push_back(&unaGranota);
 
 	Gat unGat;
+	unGat.nom("Gat Amb Botes");
 	animals.push_back(&unGat);
+
 
 	// iterem fent servir iteradors
 	for (Animals::iterator it=animals.begin(); it!=animals.end(); it++)
+	{
+		std::cout << "l'animal anomenat " << (*it)->nom() << " es de l'especie ";
 		(*it)->escriuEspecie();
+	}
 
 	return 0;
 }
