9ba535d4d98095a5440168eb93a1a3528c125d1c
guinii <guinartarnau@gmail.com>
2016-01-20 13:23:05 +0100

    Paso 03 tutorial c++

--- tutorial/exemple_polimorfisme.cxx
+++ tutorial/exemple_polimorfisme.cxx
@@ -1,16 +1,17 @@
 /* 
-  - definició d'una classe
-  - instaciacio i missatge a un objecte
+  - classe acabada en ;
+  - mètode public:
  */
 #include <iostream>
 
 class Animal
 {
+public:
 	void escriuEspecie()
 	{
 		std::cout << "...indefinida..." << std::endl;
 	}
-}
+};
 
 int main(void)
 {
@@ -20,9 +21,3 @@ int main(void)
 	return 0;
 }
 
-/*
-   Problemes: 
-   no ens compila per dos detalls importants:
-   - la declaració d'una classe en C++ ha d'acabar en ';'
-   - per defecte, els mètodes i atributs són privats. Cal fer-lo públic.
- */
