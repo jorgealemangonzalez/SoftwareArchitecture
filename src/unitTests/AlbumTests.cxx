39b2f66d6f9366486d20fd291b37e9c981d460c1
wisdompoet <wisdompoet@hotmail.com>
2016-01-28 14:26:21 +0100

    committed files for Lab 2

---
+++ src/unitTests/AlbumTests.cxx
@@ -0,0 +1,49 @@
+#include "MiniCppUnit.hxx"
+
+class AlbumTests : public TestFixture<AlbumTests>
+{
+public:
+	TEST_FIXTURE( AlbumTests )
+	{
+		//TEST_CASE( testTitle_byDefault );
+		//TEST_CASE( testTitle_afterModifying );
+		//TEST_CASE( testListed_byDefault );
+		//TEST_CASE( testListed_afterListing );
+		//TEST_CASE( testListed_afterUnlisting );
+	}
+	/*
+	void testTitle_byDefault()
+	{
+		Album album;
+		ASSERT_EQUALS( "-- Untitled --", album.title() );
+	}
+	void testTitle_afterModifying()
+	{
+		Album album;
+		album.title( "A title" );
+		ASSERT_EQUALS( "A title", album.title() );
+	}
+	void testListed_byDefault()
+	{
+		Album album;
+		ASSERT_EQUALS( false, album.isListed() );
+	}
+	void testListed_afterListing()
+	{
+		Album album;
+		album.list();
+		ASSERT_EQUALS( true, album.isListed() );
+	}
+	void testListed_afterUnlisting()
+	{
+		Album album;
+		album.list();
+		album.unlist();
+		ASSERT_EQUALS( false, album.isListed() );
+	}
+	*/
+};
+
+REGISTER_FIXTURE( AlbumTests )
+
+
---
+++ src/unitTests/TrackTests.cxx
@@ -0,0 +1,54 @@
+#include "MiniCppUnit.hxx"
+
+class TrackTests : public TestFixture<TrackTests>
+{
+public:
+	TEST_FIXTURE( TrackTests )
+	{
+		//TEST_CASE( testTitle_byDefault );
+		//TEST_CASE( testTitle_afterModifying );
+		//TEST_CASE( testDuration_byDefault );
+		//TEST_CASE( testDuration_afterModifying );
+		//TEST_CASE( testMaster_byDefault );
+		//TEST_CASE( testMaster_afterModifying );
+	}
+	/*
+	void testTitle_byDefault()
+	{
+		Track track;
+		ASSERT_EQUALS( "-- Untitled --", track.title() );
+	}
+	void testTitle_afterModifying()
+	{
+		Track track;
+		track.title( "A Title" );
+		ASSERT_EQUALS( "A Title", track.title() );
+	}
+	void testDuration_byDefault()
+	{
+		Track track;
+		ASSERT_EQUALS( 0u, track.duration() );
+	}
+	void testDuration_afterModifying()
+	{
+		Track track;
+		track.duration( 120u );
+		ASSERT_EQUALS( 120u, track.duration() );
+	}
+	void testMaster_byDefault()
+	{
+		Track track;
+		ASSERT_EQUALS( "", track.master() );
+	}
+	void testMaster_afterModifying()
+	{
+		Track track;
+		track.master( "masters/AFile.wav" );
+		ASSERT_EQUALS( "masters/AFile.wav", track.master() );
+	}
+	*/
+};
+
+REGISTER_FIXTURE( TrackTests )
+
+
