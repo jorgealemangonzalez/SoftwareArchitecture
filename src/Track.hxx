a34d064dfa28f202fac39b3c1e0c3890e64d7340
guini <arnau.guinart01@estudiant.upf.edu>
2016-02-03 23:13:19 +0100

    Track: red testDuration_afterModifying

--- src/Track.hxx
+++ src/Track.hxx
@@ -8,11 +8,18 @@ public:
         return Title;
     }
     void title(const std::string& _title){ //we set the value of a title in out private variable Title
-    	Title=_title;
+    	Title =_title;
     }
     unsigned int duration(){ //we declare the integrer as "unsigned", that's the meaning of the sufix "u".
-        return 0;
+        if(_duration != -1){ //(have to solve the problem with valgrind with variables):.
+            return _duration;
+        }
+        //return 0;		discoment for pass the test
+    }
+    void duration(unsigned int durat){
+        _duration = durat;
     }
 private:
 	std::string Title;
-};
+    unsigned int _duration=-1;
+};  
