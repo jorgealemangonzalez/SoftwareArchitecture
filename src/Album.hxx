b553450b49bb2f0d2f166aa4d09903e8ea23fbc9
jorge <jorge.alemangonzalez@gmail.com>
2016-02-08 12:00:31 +0100

    Album: refactor TitleafterModifying

--- src/Album.hxx
+++ src/Album.hxx
@@ -1,7 +1,7 @@
 class Album{
 public:
     std::string title(const std::string Title = "-- Untitled --"){//Get the title of the album
-        if(this->Title == "" || this->Title == "-- Untitled --")
+        if(this->Title == "" || this->Title == "-- Untitled --")//If the title is not set we set it to the value of the parameter
        	{
        		this->Title = Title;
        	}
@@ -11,5 +11,5 @@ public:
     	return false;
     }
 private:
-	std::string Title;
+	std::string Title;//Title of the album
 };
