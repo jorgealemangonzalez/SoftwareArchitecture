7d856372ded789c3c9725d87715b4c8958418c63
jorge <jorge.alemangonzalez@gmail.com>
2016-02-08 12:28:01 +0100

    Album refactor atributes to fit coding standards

--- src/Album.hxx
+++ src/Album.hxx
@@ -1,22 +1,22 @@
 class Album{
 public:
     std::string title(const std::string Title = "-- Untitled --"){//Get the title of the album
-        if(this->Title == "" || this->Title == "-- Untitled --")//If the title is not set we set it to the value of the parameter
+        if(_title == "" || _title == "-- Untitled --")//If the title is not set we set it to the value of the parameter
        	{
-       		this->Title = Title;
+       		_title = Title;
        	}
-       	return this->Title;
+       	return _title;
     }
     void list(){ // changes the status of the album to "Listed"
-   		Listed = true;
+   		_listed = true;
     }
     void unlist(){// changes the status of the album to "unListed"
-    	Listed = false;
+    	_listed = false;
     }
     bool isListed(){//Return if the album is listed
-    	return Listed;
+    	return _listed;
     }
 private:
-	std::string Title;//Title of the album
-	bool Listed = false;//Album is listed : true , or not : false
+	std::string _title;//Title of the album
+	bool _listed = false;//Album is listed : true , or not : false
 };
