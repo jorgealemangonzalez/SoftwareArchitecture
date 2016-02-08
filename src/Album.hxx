280875514d61107c4d453a418410acd357beb0aa
jorge <jorge.alemangonzalez@gmail.com>
2016-02-08 11:51:39 +0100

    Album: green TitleafterModifying

--- src/Album.hxx
+++ src/Album.hxx
@@ -1,6 +1,12 @@
 class Album{
 public:
     std::string title(const std::string Title = "-- Untitled --"){//Get the title of the album
-        return "-- Untitled --";
+        if(this->Title == "" || this->Title == "-- Untitled --")
+       	{
+       		this->Title = Title;
+       	}
+       	return this->Title;
     }
+private:
+	std::string Title;
 };
--- src/unitTests/AlbumTests.cxx
+++ src/unitTests/AlbumTests.cxx
@@ -5,26 +5,26 @@ class AlbumTests : public TestFixture<AlbumTests>
 public:
 	TEST_FIXTURE( AlbumTests )
 	{
-		//TEST_CASE( testTitle_byDefault );
-		//TEST_CASE( testTitle_afterModifying );
+		TEST_CASE( testTitle_byDefault );
+		TEST_CASE( testTitle_afterModifying );
 		//TEST_CASE( testListed_byDefault );
 		//TEST_CASE( testListed_afterListing );
 		//TEST_CASE( testListed_afterUnlisting );
 	}
-    /*    
+    
 	void testTitle_byDefault()
 	{
 		Album album;
 		ASSERT_EQUALS( "-- Untitled --", album.title() );
 	}
-        
+    
 	void testTitle_afterModifying()
 	{
 		Album album;
 		album.title( "A title" );
 		ASSERT_EQUALS( "A title", album.title() );
 	}
-    
+    /*
 	void testListed_byDefault()
 	{
 		Album album;
