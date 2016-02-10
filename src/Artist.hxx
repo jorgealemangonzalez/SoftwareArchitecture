88f22e6be2a51665a9fec076883a85a54161e9dc
jorge <jorge.alemangonzalez@gmail.com>
2016-02-10 15:32:30 +0000

    Artist: green testName_afterModigying

--- src/Artist.hxx
+++ src/Artist.hxx
@@ -1,6 +1,11 @@
 class Artist{
 public:
 	std::string name(const std::string &setName = "-- No name --"){//returns the name of the artist
-		return "-- No name --";
+		if(_name == "" || _name == "--No name--"){
+			_name = setName;
+		}
+		return _name;
 	}
+private:
+	std::string _name;
 };
