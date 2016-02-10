986c9020273818406aca4c08e03e148101582833
jorge <jorge.alemangonzalez@gmail.com>
2016-02-10 15:24:40 +0000

    Artist: red testName_afterModifying & changed to functions in ArtistTests because of errors

--- src/Artist.hxx
+++ src/Artist.hxx
@@ -1,6 +1,6 @@
 class Artist{
 public:
-	std::string name(){//returns the name of the artist
+	std::string name(const std::string &setName = "-- No name --"){//returns the name of the artist
 		return "-- No name --";
 	}
 };
--- src/unitTests/ArtistTests.cxx
+++ src/unitTests/ArtistTests.cxx
@@ -6,7 +6,7 @@ public:
 	TEST_FIXTURE( ArtistTests )
 	{
 		TEST_CASE(testName_byDefault);
-		//TEST_CASE(testName_afterModifying);
+		TEST_CASE(testName_afterModifying);
 		//TEST_CASE(testArtistOrGroup_byDefault);
 		//TEST_CASE(testArtistOrGroup_afterCreateGroup);
 		//TEST_CASE(testArtistOrGroup_afterDisolveGroup);
@@ -18,32 +18,33 @@ public:
 		Artist artist;
 		ASSERT_EQUALS("-- No name --",artist.name());
 	}
-	/*
-	void testName_afterModifying{
+	
+	void testName_afterModifying(){
 		Artist artist;
 		artist.name("Javi");
 		ASSERT_EQUALS("Javi",artist.name());
 	}
-	void testArtistOrGroup_byDefault{
+	/*
+	void testArtistOrGroup_byDefault(){
 		Artist artist;
 		ASSERT_EQUALS(false,artist.isGroup());
 	}
-	void testArtistOrGroup_afterCreateGroup{
+	void testArtistOrGroup_afterCreateGroup(){
 		Artist artist;
 		artist.createGroup();
 		ASSERT_EQUALS(true,artist.isGroup());
 	}
-	void testArtistOrGroup_afterDisolveGroup{
+	void testArtistOrGroup_afterDisolveGroup(){
 		Artist artist;
 		artist.createGroup();
 		artist.disolveGroup();
 		ASSERT_EQUALS(false,artist.isGroup());
 	}
-	void testDescription_byDefault{
+	void testDescription_byDefault(){
 		Artist artist;
 		ASSERT_EQUALS("-- No name -- [solo]\n",artist.description());
 	}
-	void testDescription_afterModifying{
+	void testDescription_afterModifying(){
 		Artist artist;
 		artist.name("Javi");
 		artist.createGroup();
