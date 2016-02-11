82530c422ccfbd5f03918f1a693173fc6a584f71
jorge <jorge.alemangonzalez@gmail.com>
2016-02-11 15:12:18 +0000

    Artist: green testDescription_byDefault

--- src/Artist.hxx
+++ src/Artist.hxx
@@ -11,7 +11,7 @@ public:
 		return _name;
 	}
 	std::string description(){
-		return "";
+		return "-- No name -- [solo]\n";
 	}
 	void createGroup(){//Transform the artist account into a group account
 		_grouped = true;
