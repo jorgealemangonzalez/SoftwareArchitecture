933ff3adb9dd8b592e567cc29e006e25d125afc0
jorge <jorge.alemangonzalez@gmail.com>
2016-02-11 14:59:43 +0000

    Artist: refactor testArtistOrGroup_afterCreateGroup

--- src/Artist.hxx
+++ src/Artist.hxx
@@ -1,8 +1,8 @@
 class Artist{
 public:
 	Artist(){
-		_name = "-- No name --";
-		_grouped = false;
+		_name = "-- No name --";//Set the default name 
+		_grouped = false;//Set the default kind of account
 	}
 	std::string name(const std::string &setName = "-- No name --"){//returns the name of the artist
 		if(setName != "-- No name --"){				   //Compare if the name is set
@@ -10,7 +10,7 @@ public:
 		}
 		return _name;
 	}
-	void createGroup(){
+	void createGroup(){//Transform the artist account into a group account
 		_grouped = true;
 	}
 	bool isGroup(){//retuns if the entity is an artist or a group
@@ -18,5 +18,5 @@ public:
 	}
 private:
 	std::string _name;// The name of the artist
-	bool _grouped;
+	bool _grouped;// If the account is of an artist or of a group
 };
