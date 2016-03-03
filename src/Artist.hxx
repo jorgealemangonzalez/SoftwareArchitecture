52aaf1b1670d73e45c41a14401be6b0a7a582f8d
guini <arnau.guinart01@estudiant.upf.edu>
2016-03-03 18:31:36 +0100

    REFACTOR Artist::testDescriptionCatalog_withTrackAndAlbum

--- src/Artist.hxx
+++ src/Artist.hxx
@@ -104,8 +104,8 @@ public:
     		throw e;
     	}
     }
-    /*const*/ std::string descriptionCatalog(){ //if a catalog it's empty it throws the error correct
-    	return description() + catalogTracks() + catalogAlbum(); 
+    const std::string descriptionCatalog(){ //if a catalog it's empty it throws the error correct
+    	return description() + catalogTracks() + catalogAlbum(); // The description of the catalog is the result of the convination of the artist description and catalogTracks
     }
     Album & findAlbum(const std::string &nameAlbum){
         try{
