55cd72a2a3352f68100a8212aba72c0ab4550bde
jorge <jorge.alemangonzalez@gmail.com>
2016-04-11 12:35:34 +0000

    GREEN SingAlongTests::testCatalog_withSoloArtist

--- src/SingAlong.hxx
+++ src/SingAlong.hxx
@@ -1,14 +1,34 @@
+#include "Artist.hxx"
+#include<vector>
+
 class SingAlong{
 public:
     SingAlong(){
         
     }
-    
+    ~SingAlong(){
+        std::vector<Artist*>::iterator it;
+        for(it = _catalog.begin();it != _catalog.end() ; ++it){
+            delete(*it);
+        } 
+    }
     std::string catalog(){          //Returns a list of artist
-        return "";
+        std::vector<Artist*>::iterator it;
+        std::string result("");
+        for(it = _catalog.begin();it != _catalog.end() ; ++it){
+            result+= (*it)->description();
+        }
+        return result;
     }
     
-    void createArtist(const std::string &artista , bool isGroup){
-        
+    void createArtist(const std::string &artist , bool isGroup){
+        Artist *a = new Artist();
+        a->name(artist);
+        if(isGroup){
+            a->createGroup();
+        }
+        _catalog.push_back(a);
     }
+private:
+    std::vector<Artist*> _catalog;
 };
