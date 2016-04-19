9bc78d9ad2302d5147dc8aa8473a998b0461633e
guini <arnau.guinart01@estudiant.upf.edu>
2016-04-19 11:40:09 +0200

    GREEN Mp3ConverterTests::testConvert_generateContent

--- src/Mp3Converter.hxx
+++ src/Mp3Converter.hxx
@@ -13,19 +13,16 @@ public:
 	void conver(const std::string &file, const std::string &compressedFile){
 		std::string filecompres = compressedFile + " [128].mp3";	
 		std::ofstream newfile(filecompres.c_str());
-		std::ofstream myfile;
-		myfile.open(filecompres.c_str());
-		myfile << "MP3 extracted from’masters/Master.wav’ at 128 bps and length 50s.\n";
-		myfile.close();
+		writeToFile(filecompres);
 	}
 
-	/*void writeToFile(const std::string &text, const std::string &nameFile){
+	void writeToFile(const std::string &nameFile){
         std::ofstream infile;
         infile.open(nameFile.c_str());
         if(!infile.is_open()){
             throw masterDoesNotExist();
         }
-        infile.write(text,300);
+        infile << "MP3 extracted from ’masters/Master.wav’ at 128 bps and length 50s.\n";
         infile.close();
-    }*/
+    }
 };
