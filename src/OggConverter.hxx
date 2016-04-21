d91c4ed0978f411b059eded646247626b07f9251
jorge <jorge.alemangonzalez@gmail.com>
2016-04-21 18:55:27 +0000

    REFACTOR OggConverterTests::testConvert_generateFile

--- src/OggConverter.hxx
+++ src/OggConverter.hxx
@@ -2,12 +2,12 @@
 #define OGGCONVERTER_HXX
 #include "Converter.hxx"
 #include "externalLibs/OggVorbisEncoder.hxx"
-class OggConverter:public Converter
+class OggConverter:public Converter         //inherit the data and methods from the generic converter
 {
 public:
     OggConverter(){}
     ~OggConverter(){}
-    virtual void convert(const std::string &file, const std::string &compressedFile){
+    virtual void convert(const std::string &file, const std::string &compressedFile){       //Use the OggVorbisEnconder to compress .wav to .ogg
         OGG::compress( file.c_str() , (compressedFile  + " [128].ogg").c_str() , 128 );
     }
 };
