282f88cec39e0560adf98eb6f78f6f661cf40a82
jorge <jorge.alemangonzalez@gmail.com>
2016-04-21 09:13:20 +0000

    GREEN Mp3ConverterTests::testConvert_withDifferentBitrate

--- src/Mp3Converter.hxx
+++ src/Mp3Converter.hxx
@@ -8,14 +8,21 @@
 class Mp3Converter
 {
 public:   
-	Mp3Converter(){
+	Mp3Converter():bps(128){
 
 	}
 	void conver(const std::string &file, const std::string &compressedFile){
-		std::string filecompres = compressedFile + " [128].mp3";            //put the bps of the file
-		frk_MP3_compression( file.c_str() , filecompres.c_str(), bps128);   //Call to external lib
+		//std::string filecompres = compressedFile + " ["+ std::to_string(bps) +"].mp3";            //put the bps of the file
+		switch(bps){
+		    case 96:    frk_MP3_compression( file.c_str() , (compressedFile  + " [96].mp3").c_str() , bps96);break;   //Call to external lib
+		    case 128:   frk_MP3_compression( file.c_str() , (compressedFile  + " [128].mp3").c_str(), bps128);break;   //Call to external lib
+		    case 192:   frk_MP3_compression( file.c_str() , (compressedFile + " [192].mp3").c_str(), bps192);break;   //Call to external lib
+		    case 240:   frk_MP3_compression( file.c_str() , (compressedFile + " [240].mp3").c_str(), bps240);break;   //Call to external lib
+		}
 	}
 	void bitRate(int bps){
-	    
+	    this->bps = bps;
 	}
+private:
+    int bps;
 };
