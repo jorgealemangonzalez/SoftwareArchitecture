2ded5474158f365966d51feb61478ab425eba954
jorge <jorge.alemangonzalez@gmail.com>
2016-04-21 19:07:54 +0000

    REFACTOR OggConverterTests::testConvert_generateContent

--- src/OggConverter.hxx
+++ src/OggConverter.hxx
@@ -8,7 +8,7 @@ public:
     OggConverter(){}
     ~OggConverter(){}
     virtual void convert(const std::string &file, const std::string &compressedFile){       //Use the OggVorbisEnconder to compress .wav to .ogg
-        OGG::compress( file.c_str() , (compressedFile  + " [128].ogg").c_str() , 128 );
+        OGG::compress( file.c_str() , (compressedFile  + " [128].ogg").c_str() , 128 );     //Call to the library
     }
 };
 #endif
