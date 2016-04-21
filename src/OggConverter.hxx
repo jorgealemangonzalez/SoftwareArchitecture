3a3962792eb59ff542ba1a858fd67c6a94dcb239
jorge <jorge.alemangonzalez@gmail.com>
2016-04-21 18:51:32 +0000

    GREEN OggConverterTests::testConvert_generateFile

--- src/OggConverter.hxx
+++ src/OggConverter.hxx
@@ -1,13 +1,14 @@
 #ifndef OGGCONVERTER_HXX
 #define OGGCONVERTER_HXX
 #include "Converter.hxx"
+#include "externalLibs/OggVorbisEncoder.hxx"
 class OggConverter:public Converter
 {
 public:
     OggConverter(){}
     ~OggConverter(){}
     virtual void convert(const std::string &file, const std::string &compressedFile){
-        
+        OGG::compress( file.c_str() , (compressedFile  + " [128].ogg").c_str() , 128 );
     }
 };
 #endif
