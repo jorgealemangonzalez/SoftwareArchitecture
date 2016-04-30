f9322ee49cb204797e1cffda3db6562868ae53c3
guinii <arnau.guinart01@estudiant.upf.edu>
2016-04-30 19:06:09 +0200

    GREEN ConverterGroupTests::testConvert_withMp3Converter

---
+++ src/unitTests/ConverterGroupTests.cxx
@@ -0,0 +1,57 @@
+#include "MiniCppUnit.hxx"
+#include "ConverterGroup.hxx"
+#include "LibFileSystem.hxx"
+#include <fstream>
+#include "OggConverter.hxx"
+#include "Converter.hxx"
+//#include "Mp3Converter.hxx"
+
+
+class ConverterGroupTests : public TestFixture<ConverterGroupTests>
+{
+public: 
+	TEST_FIXTURE(ConverterGroupTests)
+	{
+		TEST_CASE(testConvert_withMp3Converter);
+	}
+
+	void setUp()
+	{
+		LibFileSystem::createNetDirectory( "masters" );
+		LibFileSystem::createNetDirectory( "compressed" );
+		LibFileSystem::createNetDirectory( "config" );
+	}
+
+	/**
+	 * tearDown is automatically called after each test case
+	 */
+	void tearDown()
+	{
+		LibFileSystem::cleanupDirectory( "masters" );
+		LibFileSystem::cleanupDirectory( "compressed" );
+		LibFileSystem::cleanupDirectory( "config" );
+	}
+
+	void createMasterFile( const std::string & name, int duration )
+	{
+		std::string completeName( "masters/" );
+		completeName += name;
+		std::ofstream os( completeName.c_str() );
+		os << duration << std::endl;
+	}
+
+	void testConvert_withMp3Converter(){
+		ConverterGroup converter ;
+		createMasterFile("Master.wav",50);
+		converter.addConverter("mp3" , 128);
+		converter.convert("masters/Master.wav","compressed/Prefix","mp3",128);
+		ASSERT_EQUALS(
+			"MP3 extracted from 'masters/Master.wav' at 128 bps and length 50s.\n",
+			LibFileSystem::fileContent( "compressed/Prefix [128].mp3" )
+		);
+
+	}
+};
+
+
+REGISTER_FIXTURE( ConverterGroupTests )
