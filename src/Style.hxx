44fd6410be69f3e21ba7858d1d46d677fe01eecf
guini <arnau.guinart01@estudiant.upf.edu>
2016-05-04 17:12:33 +0200

    RED StyleTest::testName_byInitialize

---
+++ src/Style.hxx
@@ -0,0 +1,13 @@
+#include<string>
+
+class Style{
+public:
+	Style(const std::string name){
+
+	}
+	const std::string getName(){
+		return "";
+	}
+private:
+	std::string _name;
+};
---
+++ src/unitTests/StyleTest.cxx
@@ -0,0 +1,30 @@
+#include "MiniCppUnit.hxx"
+#include "Style.hxx"
+class StyleTest : public TestFixture<StyleTest>
+{
+public:
+	TEST_FIXTURE( StyleTest )
+	{
+		TEST_CASE( testName_byInitialize );
+		//TEST_CASE( testName_afterModifying );
+
+	}
+	
+	void testName_byInitialize()
+	{
+		Style style("rock");
+		ASSERT_EQUALS( "rock", style.getName() );
+	}
+	/*
+	void testName_afterModifying()
+	{
+		Style style("rock");
+		style.setName("blues");
+		ASSERT_EQUALS( "blues", style.getName() );
+	}
+	*/
+};
+
+REGISTER_FIXTURE( StyleTest )
+
+
