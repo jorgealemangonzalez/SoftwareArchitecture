d5c7b395ec575da0165fe8d3387cc235445e3e00
guini <arnau.guinart01@estudiant.upf.edu>
2016-05-04 16:32:57 +0200

    RED testStyleList_withTwoStyles

--- src/functionalTests/SubscriptionTests.cxx
+++ src/functionalTests/SubscriptionTests.cxx
@@ -10,7 +10,7 @@ public:
 	TEST_FIXTURE( SubscriptionTests )
 	{
 		TEST_CASE( testStyleList_withOneStyle );
-		//TEST_CASE( testStyleList_withTwoStyles );
+		TEST_CASE( testStyleList_withTwoStyles );
 		//TEST_CASE( testAssociateStyleWithTrack_whenStyleDoesNotExist );
 		//TEST_CASE( testCatalog_trackWithTwoStyles );
 		//TEST_CASE( testUserList_withOneUser );
@@ -55,7 +55,7 @@ public:
 		business.createNewStyle( "rumba" );
 		ASSERT_EQUALS("rumba\n", business.styleList() );
 	}
-	/*
+	
 	void testStyleList_withTwoStyles()
 	{
 		SingAlong business;
@@ -67,6 +67,7 @@ public:
 			business.styleList()
 		);
 	}
+	/*
 	void testAssociateStyleWithTrack_whenStyleDoesNotExist()
 	{
 		SingAlong business;
