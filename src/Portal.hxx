d25be338b0d9fe6f89a3075783b3834b8bf04fd1
jorge <jorge.alemangonzalez@gmail.com>
2016-06-03 18:22:16 +0000

    REFACTOR PortalTest::portalTest_default

--- src/Portal.hxx
+++ src/Portal.hxx
@@ -2,15 +2,15 @@
 #define PORTAL_HXX
 
 #include<string>
-class Portal{
+class Portal{                           //Portal for group of fans
 public:
     Portal(){}
     
-    const std::string name(){
+    const std::string name(){           //returns the name of the portal
         return "NONE";
     }
     
-    const std::string description(){
+    const std::string description(){    //returns the description of the portal
         return "NODESCRIPTION";
     }
 };
